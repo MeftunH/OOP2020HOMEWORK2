35f3d63220035c1d187d62b8062c176e03934782
Meftun Hesimli <48466124+MeftunH@users.noreply.github.com>
2020-05-13 01:17:56 +0300

    files uploaded

---
+++ NDP_ODEV2/NDP_ODEV2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace NDP_ODEV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.txty = new System.Windows.Forms.TextBox();
+            this.btnarkadasmi = new System.Windows.Forms.Button();
+            this.btnson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y";
+            // 
+            // txtx
+            // 
+            this.txtx.Location = new System.Drawing.Point(57, 76);
+            this.txtx.Name = "txtx";
+            this.txtx.Size = new System.Drawing.Size(128, 22);
+            this.txtx.TabIndex = 2;
+            this.txtx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtx_KeyPress);
+            // 
+            // txty
+            // 
+            this.txty.Location = new System.Drawing.Point(57, 155);
+            this.txty.Name = "txty";
+            this.txty.Size = new System.Drawing.Size(128, 22);
+            this.txty.TabIndex = 3;
+            this.txty.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txty_KeyPress);
+            // 
+            // btnarkadasmi
+            // 
+            this.btnarkadasmi.Location = new System.Drawing.Point(12, 227);
+            this.btnarkadasmi.Name = "btnarkadasmi";
+            this.btnarkadasmi.Size = new System.Drawing.Size(107, 45);
+            this.btnarkadasmi.TabIndex = 4;
+            this.btnarkadasmi.Text = "Arkadas mi";
+            this.btnarkadasmi.UseVisualStyleBackColor = true;
+            this.btnarkadasmi.Click += new System.EventHandler(this.btnarkadasmi_Click);
+            // 
+            // btnson
+            // 
+            this.btnson.Location = new System.Drawing.Point(170, 227);
+            this.btnson.Name = "btnson";
+            this.btnson.Size = new System.Drawing.Size(107, 45);
+            this.btnson.TabIndex = 5;
+            this.btnson.Text = "Son";
+            this.btnson.UseVisualStyleBackColor = true;
+            this.btnson.Click += new System.EventHandler(this.btnson_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 288);
+            this.Controls.Add(this.btnson);
+            this.Controls.Add(this.btnarkadasmi);
+            this.Controls.Add(this.txty);
+            this.Controls.Add(this.txtx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.TextBox txty;
+        private System.Windows.Forms.Button btnarkadasmi;
+        private System.Windows.Forms.Button btnson;
+    }
+}
+
---
+++ NDP_ODEV2/NDP_ODEV2/Form1.cs
@@ -0,0 +1,190 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2019-2020 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........:2
+**				ÖĞRENCİ ADI............:MAFTUN HASHIMLI
+**				ÖĞRENCİ NUMARASI.......:G181210554
+**                         DERSİN ALINDIĞI GRUP...:2C
+****************************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDP_ODEV2
+{
+    public partial class Form1 : Form
+    {
+        //X VE Y DEGERLERINE SADECE RAKAMGIRILE BILSIN
+        private void txtx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txty_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(312, 335);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool kontrol = true;
+        bool listekontrol = false;
+         int x = 0;
+        int y=0;
+        //EK 3 Label, 2 ListBox ve 2 TextBox ekliyoruz
+        ListBox lstboxX;
+        ListBox lstboxY;
+        TextBox txtcarpanlarintoplamiX;
+        TextBox txtcarpanlarintoplamiY;
+        Label lblx;
+            Label lbly;
+        private void btnarkadasmi_Click(object sender, EventArgs e)
+        {
+             
+            if (kontrol == true)
+            {    
+                lstboxX= new ListBox();
+                lstboxY = new ListBox();
+                txtcarpanlarintoplamiX = new TextBox();
+                txtcarpanlarintoplamiY = new TextBox();
+                lblx= new Label(); 
+                lbly= new Label();
+                //onlarin CONTROLLERINI EKLIYORUZ
+                this.AutoSize = true;
+                //KONTROLLERIN SADECE 1 KERE EKLENMESI ICIN
+                kontrol = false;
+              
+                lblx.Size = new Size(21, 20);
+                lblx.Text = "X";
+                lblx.Location = new Point(314, 70);
+                lblx.Font = new Font(lblx.Font.Name, 10.2f, FontStyle.Bold);
+                this.Controls.Add(lblx);
+
+              
+                lbly.Size = new Size(21, 20);
+                lbly.Text = "Y";
+                lbly.Location = new Point(440, 70);
+                lbly.Font = new Font(lbly.Font.Name, 10.2f, FontStyle.Bold);
+                this.Controls.Add(lbly);
+
+
+                lstboxX.Location = new Point(270, 110);
+                lstboxX.ItemHeight = 16;
+                lstboxX.Size = new Size(107, 212);
+                this.Controls.Add(lstboxX);
+
+
+                lstboxY.Location = new Point(410, 110);
+                lstboxY.ItemHeight = 16;
+                lstboxY.Size = new Size(107, 212);
+                this.Controls.Add(lstboxY);
+
+                Label lbltoplam = new Label();
+                lbltoplam.Size = new Size(106, 20);
+                lbltoplam.Text = "TOPLAMLAR:";
+                lbltoplam.Location = new Point(150, 334);
+                lbltoplam.Font = new Font(lbltoplam.Font.Name, 10.2f, FontStyle.Bold);
+                this.Controls.Add(lbltoplam);
+
+
+                txtcarpanlarintoplamiX.Size = new Size(106, 22);
+                txtcarpanlarintoplamiX.Location = new Point(269, 335);
+                txtcarpanlarintoplamiX.ReadOnly = true;
+                this.Controls.Add(txtcarpanlarintoplamiX);
+
+
+                txtcarpanlarintoplamiY.Size = new Size(106, 22);
+                txtcarpanlarintoplamiY.Location = new Point(410, 335);
+                txtcarpanlarintoplamiY.ReadOnly = true;
+                this.Controls.Add(txtcarpanlarintoplamiY);
+                kontrol = false;
+            }
+
+           
+
+            //Her yeni deger girildingde basildiginda alanlari temizlemekicin
+            if (listekontrol==true)
+            {
+                lstboxX.Items.Clear();
+                lstboxY.Items.Clear();
+                txtcarpanlarintoplamiX.Text = "";
+                txtcarpanlarintoplamiY.Text = "";
+            }
+            //ALANLAR BOS DEGILSE VE BOSLUK DA ICERMIYORRSA ASAGDAKILERI YAP DIYORUZ
+            if (txtx != null && !string.IsNullOrWhiteSpace(txtx.Text)
+                  && txty != null && !string.IsNullOrWhiteSpace(txty.Text))
+                {
+                listekontrol = true;
+                     x = int.Parse(txtx.Text);
+                     y = int.Parse(txty.Text);
+                int carpantoplamix = 0;
+                int carpantoplamiy = 0;
+
+                //girilen X degerinin yarisina kadar dondurecegiz
+                for (int carpanx = 1; carpanx <= (x / 2); carpanx++)
+                    {
+                        //Eger carpani ise
+                        if (x % carpanx == 0)
+                        {
+                            //listboxa ekliyoruz,ve carpan toplanina ekliyoruz deger
+                            carpantoplamix += carpanx;
+                            lstboxX.Items.Add(carpanx);
+                            txtcarpanlarintoplamiX.Text = carpantoplamix.ToString();
+                        }
+                    }
+                    //girilen Y degerinin yarisina kadar dondurecegiz
+                    for (int carpany = 1; carpany <= (y / 2); carpany++)
+                    {
+                        //Eger carpani ise
+                        if (x % carpany == 0)
+                        {
+                            //listboxa ekliyoruz,ve carpan toplanina ekliyoruz deger
+                            carpantoplamiy += carpany;
+                            lstboxY.Items.Add(carpany);
+                            txtcarpanlarintoplamiY.Text = carpantoplamiy.ToString();
+                        }
+                    }
+          
+                }
+                else
+                {
+                    MessageBox.Show("Lutfen X ve Y degerlerini girin", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+               
+            }
+        
+
+        private void btnson_Click(object sender, EventArgs e)
+        {  
+            
+            this.Close();
+          
+        }
+    }
+}
